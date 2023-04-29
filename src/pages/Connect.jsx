8b55b2ec2cf9a726927d84f8cc3c7b8b8814bb6a
VitorFurio <vitorluis.babireskifurio@gmail.com>
2023-04-29 11:45:29 +0000

    add temas ao botao w3mbutton e iniciando css da pagina de concetar

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -1,15 +1,31 @@
 import {useNavigate} from "react-router-dom"
-import { Web3Button } from '@web3modal/react'
+import { Web3Button, useWeb3ModalTheme } from '@web3modal/react'
+
+import logo from '../images/infLogoBlack.png'
+import '../css/Connect.css'
 
 export default function Connection(){
 
     const navigate = useNavigate()
     const goToPage = (page)=>{navigate(page)} 
 
+    const { theme, setTheme } = useWeb3ModalTheme()
+    setTheme({
+      themeMode: 'light',
+      themeVariables: {
+        '--w3m-font-family': 'Roboto, sans-serif',
+        '--w3m-accent-color': '#8700FF',
+        '--w3m-accent-fill-color' : '#FFFFFF',
+        '--w3m-background-color' : '#8700FF',
+        // '--w3m-logo-image-url' : 'https://drive.google.com/file/d/1i9_5axHFHbeFf-LwEsr-wuzFVvHZ57nR'
+      }
+    })
+
     return(
         <>
-        <h2>Conectado...</h2>
-        <Web3Button />
+        <h2>Validador Infinity V.0.3</h2>
+        <div><img src={logo} className="logo" alt="InflogoBlack" /></div>
+        <Web3Button icon="hide" label="Conecte sua carteira!"/>
         <div><button onClick={()=>goToPage("/")}>Home</button></div>
         </>
     )
