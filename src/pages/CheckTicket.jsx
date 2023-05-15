684b0916a38a529591c176127f52a2bca15c22f2
VitorFurio <vitorluis.babireskifurio@gmail.com>
2023-05-15 23:37:53 +0000

    tentativa de arrumar bug da primeira conexão

--- src/pages/CheckTicket.jsx
+++ src/pages/CheckTicket.jsx
@@ -10,23 +10,16 @@ import ContratoABI from "../contracts/contractAbi.json"
 let validado = false;
 let success = false;
 
+// TODO: Concertar bug: a primeira conexão sempre falha.
+
 export default function CheckTicket() {
   
     const [message, setMessage] = useState("")
     const navigate = useNavigate();
     const goToPage = (page)=>{navigate(page)} 
     const { disconnect } = useDisconnect()
-    let qntd = 0
-
-    const { address, isConnecting, isDisconnected } = useAccount({
-      async onConnect({ address, connector, isReconnected }) {
-        console.log('verificando dados...',)
-      },
-      onDisconnect() {
-        console.log('Disconnected');
-        goToPage("/Home")
-      },
-    })
+    const {address} = useAccount()
+    let qntd = -1
 
     const contractRead = useContractRead({
       address: '0x18145188f281c3e0E8E0f566b2CB692Ac3576892',
@@ -39,23 +32,27 @@ export default function CheckTicket() {
         }
       },
     })
+    if(BigNumber.isBigNumber(contractRead.data)){
+      qntd = contractRead.data.toNumber()
+    }
 
     function ValidaTicket(){
-      if(BigNumber.isBigNumber(contractRead.data)){
-        qntd = contractRead.data.toNumber()
-      }
       console.log("Nfts: "+qntd)
       if(validado) return;
+      // if(qntd === -1) return;
       if(qntd>0){
         console.log("Tem NFTs")
         setMessage("Você tem "+qntd+ "NFTs");
         success=true;
-        validado=true;
+        // SUCESSO, a carteira possui NFTs
+
+        //Adicione o codigo de abertura da porta/catraca aqui!
+
       }else{
         setMessage("Você não possui NFTs.");
         success=false;
-        validado=true;
       }
+      validado=true;
     }
 
     function Desconectar(){
@@ -64,12 +61,12 @@ export default function CheckTicket() {
       success=false
       setMessage("")
       disconnect() 
+      console.log('Disconnected');
+      goToPage("/Home")
     }
 
   return (
     <>
-    {/* <p>Teste {qntd}</p> */}
-      {/* {validado ? <h3>NFTs encontradas: {nfts.join(", ")}</h3> : <h3>Validando seus tickets</h3>} */}
       {validado ? <Message message={message} success={success}/> : <Loading/>}
       {validado ? <button onClick={()=>Desconectar()}>Desconectar</button> : <p> Isso pode demorar um pouco.</p>}
     </>
